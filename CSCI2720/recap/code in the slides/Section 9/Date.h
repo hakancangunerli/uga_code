0c1fe9c88629ee93c5ea0486ef8295b33fc4824c
hakancangunerli <hakancangunerli@gmail.com>
2021-08-28 16:48:24 -0400

    added section 9

---
+++ CSCI2720/recap/code in the slides/Section 9/Date.h
@@ -0,0 +1,18 @@
+// Date.h 
+
+#include <string> 
+
+#ifndef DATE_H
+#define DATE_H
+
+class Date{
+public:
+ explicit Date(unsigned int = 1 , unsigned int = 1, unsigned int = 2000);
+ std::string toString() const;
+private:
+    unsigned int month;
+    unsigned int day;
+    unsigned int year;
+
+}; 
+#endif 
