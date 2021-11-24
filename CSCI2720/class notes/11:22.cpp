dc7088ebf9441287f62670f16f52baec09bae51b
Hakan "John" Gunerli <33205097+hakancangunerli@users.noreply.github.com>
2021-11-24 00:58:59 -0500

    Add files via upload

---
+++ CSCI2720/class notes/11:22.cpp
@@ -0,0 +1,31 @@
+/*
+
+at least one problem from graph 
+
+
+edge is a vertex pair , edge from (1,2) is the same as (2,1)
+when two vertices are related, you draw an edge
+
+multigraph -> multiple edges between two vertices
+
+ how many edges are there in a complete directed graph?
+ 
+ each vertex = v-1 edges
+ in a directed graph v*(v-1)
+ in an undirected graph it'd be v*(v-1) / 2
+
+ weighted graph -> a graph in which the each edge caries a value 
+
+
+adjacency matrix -> 2d array between vertices (storage complexity v^2), (v+v^2) cost of the adjacency matrix (v for vertices, v^2 for edges)
+linkedlist -> no need for those 0s 
+0 or infinite -> 0 for missing edges. 
+
+
+if array doesn't work, use linked list
+
+
+depth-first search -> DFS
+
+
+*/ 
---
+++ CSCI2720/class notes/11:23.cpp
@@ -0,0 +1,13 @@
+/* 
+Dfs start vertex into the stack you pop from that stack, then you mark that as visited. (slide 38) 
+
+It’s still o(v+e)  like dfs 
+Not like one is better than the other
+
+bfs uses queue.
+
+
+kruskal's algorithm : minimum spanning tree (sort all the edges)
+we're not gonna talk about the implementation 
+
+/* 
