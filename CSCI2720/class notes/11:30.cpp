ae5384b42bc8ce53d8c01105ce889fb4d31b9d15
Hakan "John" Gunerli <33205097+hakancangunerli@users.noreply.github.com>
2021-11-30 19:13:50 -0500

    final notes

---
+++ CSCI2720/class notes/11:30.cpp
@@ -0,0 +1,17 @@
+/* recap 
+
+15 questions 
+
+first 10 are true and false, fill in the blanks multiple choice 
+-recursion , function tell the output 
+-rbt properties 
+-sorting algorithm 
+1 avl tree , give some data and you have to construct 
+1 hashing, hashfunction, what collision technique do you have to use? add numbers on the table  
+1 red-black tree
+1 graph, bfs dfs, three algos from those 
+1 application , what data structure can you use for that ( you have to justify)
+
+hashing: bucketing, chaining 
+
+/*
